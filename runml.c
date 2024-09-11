8833151592bd2f2b54243d7a3270e8c97c4002e5
stewmanship <97307282+tony-stew@users.noreply.github.com>
2024-09-11 18:59:36 +0800

    reer2

--- runml.c
+++ runml.c
@@ -1,7 +1,7 @@
 //  CITS2002 Project 1 2024
 //  Student1:   24214099   Lucan McDonald
 //  Student2:   24502509   Anthony Stewart
-//  Platform:   Apple
+//  Platform:   Apple b
 
 #include <stdlib.h>
 #include <stdio.h>
