f52b4c4ae9dfd7b1f585f33a1e903a58a53c4d77
PiesOnTues <115600638+PiesOnTues@users.noreply.github.com>
2024-09-19 18:05:19 +0800

    comments so i can fucking understand ts (flatbedfreestyle type beat)

--- runml.c
+++ runml.c
@@ -1,3 +1,10 @@
+//  CITS2002 Project 1 2024
+//  Student1:   24214099   Lucan McDonald
+//  Student2:   24502509   Anthony Stewart
+//  Platform:   Apple
+
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -5,129 +12,30 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+
+
 #define LINELENGTH 256
 
-void processFunction(char *line);
+
 
 // Variable defined outside of main so it has global scope
 char compiledCode[BUFSIZ] = "";
+
 // mainCode stores all code within main function 
 char mainCode[BUFSIZ];
+
 // compiledFunc stores all code within functions 
 char compiledFunc[BUFSIZ];
 
 // Tells us if we are in a function or not
 bool inFunc = false;
 
-// processes a single line of ml
-char *processLine(char *line) {
 
-    // Stores previous word
-    char prev[50];
-
-    // stores the processed line as a whole
-    static char compiledLine[LINELENGTH];
-    compiledLine[0] = '\0'; // Initialize to empty
-
-    // Tokenizes each line of code into individual words
-    char *word = strtok(line, " ");
-
-    // Loops through the line
-    while (word != NULL) {
-
-        // Handles print statements
-        if (strcmp(word, "print") == 0) {
-                    
-            // Appends printf function for float vals
-            strcat(compiledLine, "printf(\"%f\", ");
-
-            // Appends first variable
-            word = strtok(NULL, " ");
-            if (word != NULL) {
-                strcat(compiledLine, word);
-            }
-
-            // Ends print statement if it is a one variable/number print (e.g. print 3.5) 
-            word = strtok(NULL, " ");
-            if (word == NULL) {
-                strcat(compiledLine, ");\n");
-                break;
-            }
-                
-            // If it is a operation print it appends the operation symbol, appends second variable and then closes the print
-            strcat(compiledLine, word);
-            word = strtok(NULL, " ");
-            strcat(compiledLine, word);
-            strcat(compiledLine, ");\n");
-
-        }
-
-        // Handle variable assignment
-        else if (strcmp(word, "<-") == 0) {
-
-            // Appends: "double" + variable name + "="
-            strcat(compiledLine, "double ");
-            strcat(compiledLine, prev);  
-            strcat(compiledLine, " = ");
-
-            // Generates and appends variable's assigned value
-            word = strtok(NULL, " ");
-            strcat(compiledLine, word);
-            strcat(compiledLine, ";\n");
-        }
-        // If the token isn't recognized it will simply generate the next word
-        else {
-            strncpy(prev, word, sizeof(prev) - 1);
-            word = strtok(NULL, " ");
-        }
-    
-    }
-    return compiledLine;
-}
-
-// Joins multiple functions into one consolidated file
-void processFile(FILE *file) {
-    char line[LINELENGTH];
-
-    // Adds headers
-    strcat(compiledCode, "#include <stdio.h> \n ");
-
-    // Reads file line by line
-    while (fgets(line, sizeof(line), file)) {
-        line[strcspn(line, "\n")] = '\0';  
-        printf("%s\n", line);
-        // Skip comments
-        if (strchr(line, '#') != NULL) {  
-            continue;
-        } 
-        // Checks line for the word "function" then processes the function declaration and appends to compiledFunc
-        else if (strstr(line, "function") != NULL) {
-            inFunc = true;
-            processFunction(line);
-        } 
-        // If the line(s) after the function don't contain either a tab or a blank space at the beginning 
-        // We can assume that those lines aren't part of the function and that the function has ended
-        // Once the function has ended we append the compiled function to the compiled code
-        else if ((inFunc == true) && (line[0] != ' ' || line[0] != '\t')) {
-            inFunc = false;
-            strcat(compiledCode, compiledFunc);
-            strcat(compiledCode, " }");
-        } 
-        // If we are inside a function we simply process the lines and then append them to the funciton
-        else if (inFunc == true) { 
-            strcat(compiledFunc, processLine(line));
-        }
-        strcat(mainCode, processLine(line));
-    }
-    // Appends stock C code 
-    strcat(compiledCode, "int main() { ");
-    // Appends all code within main 
-    strcat(compiledCode, mainCode);
-    strcat(compiledCode, "return 0; }\n");
-}
 
 // Processes a single function
 void processFunction(char *line) {
+
+    // reset the compiledFunc variable to a null byte for each call
     compiledFunc[0] = '\0';
 
     // Assumes that the first call occurs with the function definition line
@@ -141,7 +49,7 @@ void processFunction(char *line) {
 
     // Process function parameters
     while ((word = strtok(NULL, " ")) != NULL) {
-        strcat(compiledFunc, word);
+        strcat(compiledFunc, word); // perameter name
         strcat(compiledFunc, ", ");
     }
 
@@ -154,12 +62,138 @@ void processFunction(char *line) {
     strcat(compiledFunc, ") {\n");
 }
 
+
+
+// processes a single line of ml
+char *processLine(char *line) {
+
+    // Stores previous word
+    char prev[50];
+
+    // Stores the processed line as a whole
+    static char compiledLine[LINELENGTH];
+    // Initialize to empty
+    compiledLine[0] = '\0'; 
+
+    // Tokenizes each line of code into individual words
+    char *word = strtok(line, " ");
+
+    // Loops through the line
+    while (word != NULL) {
+
+        // Handles print statements
+        if (strcmp(word, "print") == 0) {
+                    
+            // concatenates printf function for float vals
+            strcat(compiledLine, "printf(\"%f\", ");
+
+            // concatenates first variable
+            word = strtok(NULL, " ");
+            if (word != NULL) {
+                strcat(compiledLine, word);
+            }
+
+            // Ends print statement if it is a one variable/number print (e.g. print 3.5) 
+            word = strtok(NULL, " ");
+            if (word == NULL) {
+                strcat(compiledLine, ");\n");
+                break;
+            }
+                
+            // If it is a operation print it concatenates the operation symbol, concatenates second variable and then closes the print
+            strcat(compiledLine, word);
+            word = strtok(NULL, " ");
+            strcat(compiledLine, word);
+            strcat(compiledLine, ");\n");
+
+        }
+
+        // Handle variable assignment
+        else if (strcmp(word, "<-") == 0) {
+
+            // concatenates: "double" + variable name + "="
+            strcat(compiledLine, "double ");
+            strcat(compiledLine, prev);  
+            strcat(compiledLine, " = ");
+
+            // Generates and concatenates variable's assigned value
+            word = strtok(NULL, " ");
+            strcat(compiledLine, word);
+            strcat(compiledLine, ";\n");
+        }
+
+        // If the token isn't recognized it will simply generate the next word
+        else {
+            strncpy(prev, word, sizeof(prev) - 1);
+            word = strtok(NULL, " ");
+        }
+    }
+
+    return compiledLine;
+}
+
+// Joins multiple compiled functions into one consolidated output string
+void processFile(FILE *file) {
+
+    char line[LINELENGTH];
+
+    // Adds headers
+    strcat(compiledCode, "#include <stdio.h> \n ");
+
+    // Reads file line by line
+    while (fgets(line, sizeof(line), file)) {
+
+        line[strcspn(line, "\n")] = '\0';  
+        printf("%s\n", line);
+
+        // Skip comments
+        if (strchr(line, '#') != NULL) {  
+            continue;
+        } 
+
+        // Checks line for the word "function" then processes the function declaration and concatenates to compiledFunc
+        else if (strstr(line, "function") != NULL) {
+            inFunc = true;
+            processFunction(line);
+        } 
+
+        // If the line(s) after the function don't contain either a tab or a blank space at the beginning 
+        // We can assume that those lines aren't part of the function and that the function has ended
+        // Once the function has ended we append the compiled function to the compiled code
+        else if ((inFunc == true) && (line[0] != ' ' || line[0] != '\t')) {
+            inFunc = false;
+            strcat(compiledCode, compiledFunc);
+            strcat(compiledCode, " }");
+        } 
+
+        // If we are inside a function we simply process the lines and then append them to the funciton
+        else if (inFunc == true) { 
+            strcat(compiledFunc, processLine(line));
+        }
+        strcat(mainCode, processLine(line));
+    }
+
+    // concatenates stock C code 
+    strcat(compiledCode, "int main() { ");
+
+    // concatenates all code within main 
+    strcat(compiledCode, mainCode);
+
+    // sets main to return nothing
+    strcat(compiledCode, "return 0; }\n");
+}
+
+
+
 int main(int argc, char *argv[]) {
+
+    // tony put a comment here
     if (argc != 2) { 
         fprintf(stderr, "usage: %s <input file>\n", argv[0]);
         return 1;
     }
 
+    // gets file name from command line input
     const char *file_in_path = argv[1];
 
     // Opens .ml file and then compiles it in processFile function 
@@ -168,6 +202,7 @@ int main(int argc, char *argv[]) {
         perror("error opening input file");
         return 1;
     }
+
     processFile(file_in);
     fclose(file_in);
 
