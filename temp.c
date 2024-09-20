c6179e5300bfda7ef0217a1a36cf4a8715e6a8c1
PiesOnTues <115600638+PiesOnTues@users.noreply.github.com>
2024-09-20 16:39:54 +0800

    looking at counting

--- temp.c
+++ temp.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <math.h> 
-double x = 8;
-double y = 3;
-
+double v1 = double v2 = double v3 = double v4 = double v5 = double v6 = double v7 = double v8 = double v9 = double v10 = double v11 = double v12 = double v13 = double v14 = double v15 = double v16 = double v17 = double v18 = double v19 = double v20 = double v21 = double v22 = double v23 = double v24 = double v25 = double v26 = double v27 = double v28 = double v29 = double v30 = double v31 = double v32 = double v33 = double v34 = double v35 = double v36 = double v37 = double v38 = double v39 = double v40 = double v41 = double v42 = double v43 = double v44 = double v45 = double v46 = double v47 = double v48 = double v49 = double v50 = 
 void printVal(double value) { if (value == (int)value) { printf("%d\n", (int)value); } else { printf("%f\n", value); } }
  
 int main() { 
-printVal(x*y);
 return 0;
  }
