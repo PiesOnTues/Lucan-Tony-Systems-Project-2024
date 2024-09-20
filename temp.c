8d457b48c467592c8c03a0a83f8420878ac664d6
stewmanship <97307282+tony-stew@users.noreply.github.com>
2024-09-20 15:11:53 +0800

    dcdc

--- temp.c
+++ temp.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <math.h> 
-double a = 0;
+double x = 8;
+double y = 3;
+double x = 0;
 void printVal(double value) { if (value == (int)value) { printf("%d\n", (int)value); } else { printf("%f\n", value); } }
  
-double printsum(double a, double b) { 
-	printVal(0+b);
-	return 0;
- }
 int main() { 
-printsum (12, 6);
+printVal(0*y);
 return 0;
  }
